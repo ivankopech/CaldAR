cf893691ea3bba4480f4e07ad985efb6c4e24011
ivan kopech <ivankopech@gmail.com>
2021-09-06 14:49:34 -0300

    json tecnicos ficticios

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-const http = require('http');
+// const http = require('http');
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const paginaTecnico = fs.readFileSync('tecnicos.html');
+// const paginaTecnico = fs.readFileSync('tecnicos.html');
 
-const servidor = http.createServer((request, response) => {
-    //console.log(request.url);
+// const servidor = http.createServer((request, response) => {
+//     //console.log(request.url);
     
 
-    if(request.url === '/tecnicos'){
-        return response.end(paginaTecnico);
-    } else if(request.url === '/contacto'){
-        return response.end('pagina Contacto');
-    } else if(request.url === '/'){
-        return response.end('pagina Inicio');
-    } else{
-        response.writeHead(404);
-        response.end('pagina no encontrada');
-    }
+//     if(request.url === '/tecnicos'){
+//         return response.end(paginaTecnico);
+//     } else if(request.url === '/contacto'){
+//         return response.end('pagina Contacto');
+//     } else if(request.url === '/'){
+//         return response.end('pagina Inicio');
+//     } else{
+//         response.writeHead(404);
+//         response.end('pagina no encontrada');
+//     }
     
     
-});
-servidor.listen(3000);
+// });
+// servidor.listen(3000);
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('app corriendo en puerto 3000');
+})
+
+//rutas
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'tecnicos.html'));
+})
+app.get('/tecnicos', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'tecnicos.json'));
+})
+
+app.get('/', (request, response) =>{
+    response.send({
+    nombre: 'ivan'
+    })
+})
