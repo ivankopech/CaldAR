e5b98d5cb32a8562503ad24b6d5865427211e856
ivan kopech <ivankopech@gmail.com>
2021-09-08 19:26:59 -0300

    get de tecnicos funciona

--- rutas/rutas-tecnicos.js
+++ rutas/rutas-tecnicos.js
@@ -1,1 +1,14 @@
-console.log('f');
+const express = require('express');
+const router = express.Router();
+const tecnicosControladora = require('../controladora/controladora-tecnicos.js');
+
+router.get('/constructoras', tecnicosControladora.obtenerTecnicos);
+router.get('/constructoras/buscar',tecnicosControladora.obtenerTecnicosPorApellido);
+router.get('/constructoras/:id',tecnicosControladora.obtenerTecnicoPorID);
+router.post('/constructoras',tecnicosControladora.crearTecnico);
+router.delete('/constructoras/:id', tecnicosControladora.eliminarTecnico);
+router.put('/constructoras/:id',tecnicosControladora.actualizarTecnicos);
+
+module.exports = router;
+
+
