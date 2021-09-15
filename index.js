90354a6c02ba809bf8c48b2ac1d90e3e53955ddd
ivan kopech <ivankopech@gmail.com>
2021-09-15 15:26:26 -0300

    conexion mongodb edificios

--- index.js
+++ index.js
@@ -50,30 +50,41 @@
 // servidor.listen(3000);
 
 
-const path = require('path');
-
 const express = require('express');
+const mongoose = require('mongoose');
+const rutaEdificios = require('./rutas/rutas-edificios');
 
 const app = express();
-
+app.use(express.json());
+app.use(rutaEdificios);
 
 const path = require('path');
+const puerto = 3000;
 
+mongoose.connect('mongodb+srv://JERE:JERE1234@cluster0.mukqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then((result) => {
+        console.log('base de datos conectada');
+    })
+    .catch((error) => {
+        console.log(`base de datos no conectada, error: ${error} `)
+    })
 
-app.listen(3000, () => {
-    console.log('app corriendo en puerto 3000');
+app.listen(puerto, () => {
+    console.log(`example app listening at http://localhost:${puerto}`)
 })
 
-
-//rutas
-app.get('/html', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'edificios.html'));
-})
-app.get('/edificios', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'edificios.json'));
-})
+// app.listen(3000, () => {
+//     console.log('app corriendo en puerto 3000');
+// })
 
 
+// //rutas
+// app.get('/html', (request, response) => {
+//     response.sendFile(path.resolve(__dirname, 'edificios.html'));
+// })
+// app.get('/edificios', (request, response) => {
+//     response.sendFile(path.resolve(__dirname, 'edificios.json'));
+// })
 
 
 
