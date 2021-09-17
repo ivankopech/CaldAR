b1196876099413b545867e3e9ae3365d5fd1294c
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-09-17 10:56:38 -0300

    Conectando constructoras al servidor mongoose

--- index.js
+++ index.js
@@ -33,15 +33,36 @@
     
 // });
 // servidor.listen(3000);
-
-const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const rutaConstructora = require('./rutas/constructora-rutas');
 
 const app = express();
+app.use(express.json());
+app.use(rutaConstructora);
 
-app.listen(3000, () => {
-    console.log('app corriendo en puerto 3000');
+const path = require('path');
+const puerto = 3000;
+
+mongoose.connect('mongodb+srv://JERE:JERE1234@cluster0.mukqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then((result) => {
+        console.log('base de datos conectada');
+    })
+    .catch((error) => {
+        console.log(`base de datos no conectada, error: ${error} `)
+    })
+
+app.listen(puerto, () => {
+    console.log(`example app listening at http://localhost:${puerto}`)
 })
+// const path = require('path');
+// const express = require('express');
+
+// const app = express();
+
+// app.listen(3000, () => {
+//     console.log('app corriendo en puerto 3000');
+// })
 
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'constructoras.html'));
