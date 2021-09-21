1e0910043224659df65349519c21bcff320c324c
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-09-21 17:39:24 -0300

    Agregando puertos

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const express = require('express');
 const app = express()
 
 const path = require('path')
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001, () =>{
-    console.log('Aplicacion corriendo en el puerto 3000')
+app.listen(PORT, () =>{
+    console.log('Aplicacion corriendo en el puerto 3000', PORT)
 })
 
 //Rutas
