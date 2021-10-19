7af501a8820adf784af0ba3d45392ab182ed866a
ivan kopech <ivankopech@gmail.com>
2021-10-19 15:10:56 -0300

    nombres de metodos a ingles

--- models/caldera.js
+++ models/caldera.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require('mongoose');
 
-const CalderaSchema = Schema({
+const BoilerSchema = Schema({
   nombre: {
     type: String,
     required: [true, 'The name field is required'],
   },
 });
 
-module.exports = model('Caldera', CalderaSchema);
+module.exports = model('Caldera', BoilerSchema);
